3195b5e3d66c63ff63b285adbc0dc5b72f8ff7f1
John Albert P. Recio <74884939+AlbertRecio@users.noreply.github.com>
2021-02-28 13:20:33 +0800

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,14 +1,24 @@
 <?php
-if(isset($_POST['submit'])){
+
 	$name = $_POST['name'];
 	$mailfrom = $_POST['email'];
-	$name = $_POST['message'];
+	$message = $_POST['message'];
+	$number = $_POST['number'];
+	
+	$emailSubject = "Job Information";
+
+	$mailBody = "User Name: ". $name.". \n".
+				"User Email: ". $mailfrom.".\n".
+				"User message: ". $message.".\n".
+				"User Number: ". $number.".\n";
 
 	$mailTo = "recio_albert@yahoo.com";
+
 	$headers = "From: ".$mailfrom;
 
-	$txt = "You have received an email from ". $name."\n\n".$message;
+	$headers .= "Reply To". $mailfrom;
 
-	mail($mailTo, $text, $headers);
-	header("Location: index.html?mailsend");
-}
+	mail($mailTo,$emailSubject,$mailBody,$headers);
+
+	header("Locations: index.html");
+?>
